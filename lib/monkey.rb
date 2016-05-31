74f4907b1ed2bf6cfff5109b623c9cf69b64eddb
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-05-31 16:33:52 -0700

    starting point

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -1,17 +1,4 @@
-class Monkey
-  attr_reader :name, :species, :foods_eaten
+##code here
 
-  def initialize(name, species)
-    @name = name
-    @species = species
-    @foods_eaten = []
-  end
-
-  def eat(food)
-    @foods_eaten.push(food)
-  end
-
-  def introduce
-    return "Hi, I'm #{@name}, I'm a #{@species}, and I've eaten #{@foods_eaten.join(", ")}."
-  end
-end
+# class Monkey
+# end
